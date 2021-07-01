36ce84ee8ef7559920f800824f8c543686d70601
20A95A0416 <86709293+20A95A0416@users.noreply.github.com>
2021-07-01 22:39:41 +0530

    Add files via upload

---
+++ for loop (starting,end range).py
@@ -0,0 +1,13 @@
+#for loop
+
+
+a=int(input())
+b=int(input())
+r=int(input())
+for i in range(a,b+1):
+    for j in range(1,r+1):
+        print(i,'x',j,'=',i*j)
+    print()   
+        
+    
+    
